19a2e001baaaf3a1489e4e509322dead68c475aa
ohhfishal <ohhfishal@gmail.com>
2025-07-19 14:29:19 -0700

    dev: dump progress

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "jg";
   home.homeDirectory = "/home/jg";
 
@@ -15,14 +13,13 @@
   # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
     pkgs.cowsay
     pkgs.tmux
+    pkgs.lolcat
+    pkgs.go
+    pkgs.rustc
+    pkgs.tree
 
     (pkgs.writeShellScriptBin "switch" ''
       home-manager switch
@@ -33,60 +30,37 @@
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
+    ".vim" = {
+      source = ./sources/vim;
+      recursive = true;
+    };
     ".tmux.conf" = {
-        source = ./sources/tmux.conf;
-      };
+      source = ./sources/tmux.conf;
+    };
     ".gitconfig" = {
-        source = ./sources/gitconfig;
-      };
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+      source = ./sources/gitconfig;
+    };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/jg/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
+  # Config vim
+  programs.vim = {
+    enable = true;
+    extraConfig = builtins.readFile ./sources/vimrc;
   };
+
 
   # Config Bash
   programs.bash = {
     enable = true;
     initExtra = builtins.readFile ./sources/bashrc;
+  };
+
+  # Set envs
+  home.sessionVariables = {
+    EDITOR = "vim";
   };
 
   # Let Home Manager install and manage itself.
